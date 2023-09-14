d3e878602375ef7ad0d22303d0978431f544ec5e
frankenfurth <lampsdad@tamu.edu>
2023-09-14 16:03:46 -0500

    Toggle Style
    Fixed the toggle style button to account for the second css stlye

--- button.js
+++ button.js
@@ -1,16 +1,32 @@
 let isAlternateStyle = false;
+const linkElement = document.getElementById('stylesheet');
+
+//initially hide the alternate style
+window.onload = function() {
+    var sectionToHide = document.getElementById('style2');
+    sectionToHide.style.display = "none";
+}
 
 function toggleStyle() {
-    const linkElement = document.getElementById('stylesheet');
-
+    var sectionToHide = document.getElementById('style2');
     if (isAlternateStyle) {
         linkElement.href = 'landingstyle.css'; // Use the original CSS file
+        sectionToHide.style.display = "none";
     } else {
         linkElement.href = 'landingaltstyle.css'; // Use the alternate CSS file
+        sectionToHide.style.display = "block";
     }
 
     isAlternateStyle = !isAlternateStyle; // Toggle the style state
 }
 
+function toggleOverlay() {
+    var overlay = document.querySelector('.overlay');
+    overlay.classList.toggle('active');
+}
+
 // Attach the toggle function to the button
 document.getElementById('toggleButton').addEventListener('click', toggleStyle);
+
+// Attach the overlay function to the hamburger button
+document.querySelector('.hamburger-menu').addEventListener('click', toggleOverlay);
